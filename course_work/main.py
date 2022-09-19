ec88b79510921be3e14d98324e9f8352efd97676
Skuld <nike073i@mail.ru>
2022-09-19 19:03:12 +0400

    Осталось разбить по файлам

--- course_work/main.py
+++ course_work/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 import pandas as pd
 from config import *
 from sklearn.preprocessing import LabelEncoder
@@ -6,8 +6,11 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 
 pd.options.mode.chained_assignment = None
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 cars_data = None
+classifier_tree = None
+x = []
+y = []
 
 
 def read_data(file_path, required_columns, index_col):
@@ -20,7 +23,7 @@ def prepare_data(data):
     # Выбрасываем строки с пустыми значениями
     m_data = data.dropna()
 
-    # # Числовое кодирование для трансмисии
+    # Числовое кодирование для трансмисии
     le = LabelEncoder()
     le.fit(m_data['transmission'])
     m_data['transmission'] = le.transform(m_data['transmission'])
@@ -34,15 +37,36 @@ def prepare_data(data):
     return m_data
 
 
-def print_classifier_info(feature_importances, precision):
-    feature_names = FEATURE_COLUMNS
-    scores = map(lambda score: round(score, precision), feature_importances)
-    print(dict(zip(feature_names, scores)))
+def classification(test_size, precision):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size,
+                                                        random_state=TRAIN_RANDOM_STATE)
+    classifier_tree.fit(x_train, y_train)
+    test_score = classifier_tree.score(x_test, y_test)
+    feature_scores = map(lambda score: round(score, precision), classifier_tree.feature_importances_)
+    return test_score, dict(zip(FEATURE_COLUMNS, feature_scores))
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello" if cars_data is None else str(cars_data.head(5))
+    return render_template('index.html')
+
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+
+@app.route('/get_classified')
+def get_classified():
+    try:
+        test_size = float(request.args.get('size'))
+        precision = int(request.args.get('precision'))
+    except ValueError:
+        return redirect(url_for('error'))
+
+    # Проверка входных условий
+    score, feature_importances = classification(test_size, precision)
+    return render_template('classified.html', data=dict(score=score, feature_importances=feature_importances))
 
 
 if __name__ == "__main__":
@@ -54,18 +78,7 @@ if __name__ == "__main__":
     x = cars_data[FEATURE_COLUMNS]
     y = cars_data.drop(FEATURE_COLUMNS, axis=1)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TRAIN_TEST_SIZE, random_state=TRAIN_RANDOM_STATE)
-
-    classifier_tree.fit(x_train, y_train)
-    print(classifier_tree.score(x_test, y_test))
-    print_classifier_info(classifier_tree.feature_importances_, 2)
-
     app.run(debug=False)
-
-# Вынести все в конфиг
-# Сделать работу веб-сервиса
-# Выводить по запросу html с таблицей результатов и оценкой модели.
-# Мб сделать выбор параметров через веб-сервис
 
 # Задачи по курсовой работе:
 # 1. (Нейронка и дерево решений) Выявление наиболее важного признака, влияющего на тип коробки передач в автомобиле с помощью дерева решений.
