667dd2c392dd6cff757b9d16d6acbefac8ad32a4
Nike073i <nike073i@mail.ru>
2022-10-23 23:08:57 +0400

    Небольшой рефакторинг 5-6

--- filippov_nikita_lab_5/main.py
+++ filippov_nikita_lab_5/main.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.metrics import roc_auc_score, roc_curve, classification_report
 from sklearn.model_selection import train_test_split
 from config import *
-
-pd.options.mode.chained_assignment = None
 
 
 def read_data(file_path, required_columns, n_rows):
@@ -29,24 +27,39 @@ def prepare_data(data):
     return data
 
 
-def logic_regressio(X, y):
+def logic_regression(X, y):
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=TRAIN_TS, random_state=TRAIN_RS)
-    model = LogisticRegression(random_state=REGRESSIO_RS)
+    model = LogisticRegression(random_state=REGRESSION_RS)
     model.fit(x_train, y_train)
     model_score = model.score(x_test, y_test)
-    print(model_score)
+
     logic_roc_auc = roc_auc_score(y_train, model.predict(x_train))
     fpr, tpr, thresholds = roc_curve(y_train, model.predict_proba(x_train)[:, 1])
     plt.figure()
     plt.plot(fpr, tpr, label='Точность регрессии = %0.2f' % logic_roc_auc)
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False positive rate (FPR)')
+    plt.ylabel('True positive rate (TPR)')
+    plt.title('Оценка бинарной классификации с помощью ROC-кривой')
+    plt.legend(loc='lower right')
     plt.show()
-    print(logic_roc_auc)
+
+    print("Оценка модели (средняя точность) : %0.2f" % model_score)
+    print("Площадь под кривой ROC (AUC ROC) : %0.2f" % logic_roc_auc)
+
+    print("Отчеты по качеству классификации с помощью логистической регресии")
+    print("Тестовые данные:")
+    print(classification_report(y_test, model.predict(x_test)))
+    print("Тренировочные данные:")
+    print(classification_report(y_train, model.predict(x_train)))
 
 
 if __name__ == "__main__":
     cars_data = read_data(FILE_PATH, FEATURE_COLUMNS + [TARGET_COLUMN], DATA_N_ROWS)
     prepared_data = prepare_data(cars_data)
-    logic_regressio(prepared_data[FEATURE_COLUMNS], prepared_data[TARGET_LABEL])
+    logic_regression(prepared_data[FEATURE_COLUMNS], prepared_data[TARGET_LABEL])
 
 
 # Задача прогнозирования класса стоимости а/м-ля (Премиум, бюджетные) по пробегу и году выпуска
--- filippov_nikita_lab_6/main.py
+++ filippov_nikita_lab_6/main.py
@@ -1,24 +1,7 @@
-
-# Это же задание идет в 6 лабу.
-# Завтрашний план:
-# 5:
-#     1) Прочитать необходимые данные (price, power, brand)
-#     2) Обработать данные (brand, и power. brand по числовому, а power в 2 класса)
-#     3) Взять данные, разбить на тестовые и тренировочные
-#     4) Взять лог.рес, обучить на трен, оценить на тест.
-#     5) Взять рок кривую, создать по ней график
-#     6) Вывести отчет по року
-# 6:
-#     1) Взять пункты 1-3 из 5.
-#     2) Взять млпклассифаер, обучить нейронку и проверить
-
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPRegressor, MLPClassifier
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.neural_network import MLPClassifier
+
 from config import *
 
 pd.options.mode.chained_assignment = None
@@ -47,7 +30,7 @@ def prepare_data(data):
 
 def mlp_classifier(X, y):
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=TRAIN_TS, random_state=TRAIN_RS)
-    model = MLPClassifier(random_state=MLP_RS)
+    model = MLPClassifier(random_state=MLP_RS, hidden_layer_sizes=HIDDEN_LAYER_SIZES, max_iter=MAX_ITER)
     model.fit(x_train, y_train)
     model_score = model.score(x_test, y_test)
     print(model_score)
@@ -57,7 +40,6 @@ if __name__ == "__main__":
     cars_data = read_data(FILE_PATH, FEATURE_COLUMNS + [TARGET_COLUMN], DATA_N_ROWS)
     prepared_data = prepare_data(cars_data)
     mlp_classifier(prepared_data[FEATURE_COLUMNS], prepared_data[TARGET_LABEL])
-
 
 # Задача прогнозирования класса стоимости а/м-ля (Премиум, бюджетные) по пробегу и году выпуска
 # Определение категории страхования автомобиля
