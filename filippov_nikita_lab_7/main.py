afc56f47770bbc7c2e73fe5472c91a48a56011ea
Nike073i <nike073i@mail.ru>
2022-11-19 22:01:24 +0400

    Небольшой рефакторинг

--- filippov_nikita_lab_7/main.py
+++ filippov_nikita_lab_7/main.py
@@ -8,10 +8,46 @@ from keras.layers import LSTM
 from keras.models import Sequential
 from keras.utils import np_utils
 
-filename = "Prestuplenie_i_nakazanie.txt"
+SEQ_LENGTH = 100
 
-raw_text = open(filename, 'r', encoding='utf-8').read()
-raw_text = raw_text.lower()
+
+def read_text():
+    filename = "Prestuplenie_i_nakazanie.txt"
+    text = open(filename, 'r', encoding='utf-8').read()
+    text = text.lower()
+    return text
+
+
+def split_data():
+    m_x = []
+    m_y = []
+    for i in range(0, text_size - SEQ_LENGTH, 1):
+        seq_in = raw_text[i:i + SEQ_LENGTH]
+        seq_out = raw_text[i + SEQ_LENGTH]
+        m_x.append([char_to_int[char] for char in seq_in])
+        m_y.append(char_to_int[seq_out])
+    return m_x, m_y
+
+
+def create_model(x, y):
+    filepath_weights = "weights-improvement={epoch:02d}-{loss:4f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath_weights, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    callbacks_list = [checkpoint]
+
+    m_model = Sequential()
+    m_model.add(LSTM(256, input_shape=(x.shape[1], x.shape[2]), return_sequences=True))
+    m_model.add(Dropout(0.2))
+    m_model.add(LSTM(256))
+    m_model.add(Dropout(0.2))
+    m_model.add(Dense(y.shape[1], activation='softmax'))
+
+    # model.load_weights ("")
+    m_model.compile(loss='categorical_crossentropy', optimizer='adam')
+    m_model.fit(x, y, epochs=100, batch_size=128, callbacks=callbacks_list, verbose=1)
+    return m_model
+
+
+raw_text = read_text()
 
 chars = sorted(list(set(raw_text)))
 char_to_int = dict((char, i) for i, char in enumerate(chars))
@@ -20,37 +56,15 @@ int_to_char = dict((i, char) for i, char in enumerate(chars))
 vocab_size = len(chars)
 text_size = len(raw_text)
 
-seq_length = 100
-
-x_data = []
-y_data = []
-for i in range(0, text_size - seq_length, 1):
-    seq_in = raw_text[i:i + seq_length]
-    seq_out = raw_text[i + seq_length]
-    x_data.append([char_to_int[char] for char in seq_in])
-    y_data.append(char_to_int[seq_out])
+x_data, y_data = split_data()
 
 n_patterns = len(x_data)
 
-X = np.reshape(x_data, (n_patterns, seq_length, 1))
+X = np.reshape(x_data, (n_patterns, SEQ_LENGTH, 1))
 X = X / float(vocab_size)
-
 y_data = np_utils.to_categorical(y_data)
 
-filepath_weights = "weights-improvement={epoch:02d}-{loss:4f}.hdf5"
-checkpoint = ModelCheckpoint(filepath_weights, monitor='loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
-
-model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(256))
-model.add(Dropout(0.2))
-model.add(Dense(y_data.shape[1], activation='softmax'))
-
-# model.load_weights ("")
-model.compile(loss='categorical_crossentropy', optimizer='adam')
-model.fit(X, y_data, epochs=100, batch_size=128, callbacks=callbacks_list, verbose=1)
+model = create_model(X, y_data)
 
 start = np.random.randint(0, len(x_data) - 1)
 pattern = x_data[start]
